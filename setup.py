fed6ad2d7376f0e23bb6c84acb596556c76e0a21
Kozodoi <n.kozodoi@icloud.com>
2020-04-15 12:30:18 +0200

    setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name = 'preptools',
+      version = '0.1',
+      description = 'Data Preprocessing Tools',
+      author = 'Nikita Kozodoi',
+      author_email = 'n.kozodoi@icloud.com',
+      packages = ['preptools'],
+      zip_safe = False
+     )
