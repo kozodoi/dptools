64161eb5c3954407d280bf3d41cff1dba8af2d14
Kozodoi <n.kozodoi@icloud.com>
2020-04-15 14:56:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
-setup(name = 'preptools',
-      version = '0.1',
+setup(name = 'dptools',
+      version = '0.1.0',
       description = 'Data Preprocessing Tools',
       author = 'Nikita Kozodoi',
       author_email = 'n.kozodoi@icloud.com',
-      packages = ['preptools'],
+      url = 'https://github.com/kozodoi/dptools',
+      packages = ['dptools'],
+      install_requires = ['numpy', 'pandas', 'sklearn'],
+      license = 'MIT',
       zip_safe = False
      )
