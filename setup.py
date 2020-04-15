303f7aecfbccadcb7ae5b8c07a8d850dfe3bfc83
Kozodoi <n.kozodoi@icloud.com>
2020-04-15 17:08:10 +0200

    include packade description

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name = 'dptools',
-      version = '0.2.0',
+      version = '0.2.1',
       description = 'Data Preprocessing Tools',
+      long_description = long_description,
+      long_description_content_type = 'text/markdown',
       author = 'Nikita Kozodoi',
       author_email = 'n.kozodoi@icloud.com',
       url = 'https://github.com/kozodoi/dptools',
