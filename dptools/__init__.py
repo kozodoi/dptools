44c44e8cf96d29414b9097d0453c2f6811e2794e
Nikita Kozodoi <n.kozodoi@icloud.com>
2020-04-16 15:44:34 +0200

    import new functions

--- dptools/__init__.py
+++ dptools/__init__.py
@@ -2,6 +2,7 @@ from .aggregation import aggregate_data
 
 from .feature_engineering import add_date_features
 from .feature_engineering import add_text_features
+from .feature_engineering import split_features
 
 from .treat_factors import print_factor_levels
 from .treat_factors import label_encoding
@@ -10,3 +11,5 @@ from .treat_missings import print_missings
 from .treat_missings import fill_missings
 
 from .treat_constants import find_constant_features
+
+from .save_version import save_csv_version
