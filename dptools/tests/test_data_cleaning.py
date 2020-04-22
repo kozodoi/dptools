7fed659781524ec2ee366546c97258e83e0db76d
Nikita Kozodoi <n.kozodoi@icloud.com>
2020-04-22 22:37:26 +0200

    add test for find_constant_features()

--- dptools/tests/test_data_cleaning.py
+++ dptools/tests/test_data_cleaning.py
@@ -2,5 +2,11 @@ import numpy as np
 import pandas as pd
 import pytest
 
-def test_test():
-    assert 1 == 1
+def test_find_constant_features():
+
+    data = {'age': [27, np.nan, 30, 25, np.nan], 
+        'height': [170, 168, 173, 177, 165], 
+        'gender': ['female', 'female', 'female', 'female', 'female']}
+    df = pd.DataFrame(data)
+    
+    assert find_constant_features(df) == ['gender']
