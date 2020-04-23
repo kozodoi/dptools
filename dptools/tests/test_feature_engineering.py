5af06d6dde4914e5e84eac79f5f9e39173de86f7
Kozodoi <n.kozodoi@icloud.com>
2020-04-23 09:53:46 +0200

    add tests for add_text_features()

--- dptools/tests/test_feature_engineering.py
+++ dptools/tests/test_feature_engineering.py
@@ -6,7 +6,7 @@ from dptools import add_date_features
 from dptools import add_text_features
 from dptools import aggregate_data
 
-def aggregate_data_6():
+def test_aggregate_data_6():
     data = {'age': [27, np.nan, 30, 25, np.nan], 
         'height': [170, 168, 173, 177, 165], 
         'gender': ['female', 'male', np.nan, 'male', 'female'],
@@ -18,7 +18,7 @@ def aggregate_data_6():
                         fac_stats = 'mode')   
     assert df.shape[1] == 6
 
-def aggregate_data_8():
+def test_aggregate_data_8():
     data = {'age': [27, np.nan, 30, 25, np.nan], 
         'height': [170, 168, 173, 177, 165], 
         'gender': ['female', 'male', np.nan, 'male', 'female'],
@@ -29,3 +29,27 @@ def aggregate_data_8():
                         num_stats = ['min', 'mean', 'max'], 
                         fac_stats = ['mode', 'count'])   
     assert df.shape[1] == 8
+
+def test_add_text_features_9():
+    data = {'age': [27, np.nan, 30, 25, np.nan], 
+        'height': [170, 168, 173, 177, 165], 
+        'gender': ['female', 'male', np.nan, 'male', 'female'],
+        'income': ['high', 'medium', 'low', 'low', 'no income']}
+    df = pd.DataFrame(data)
+    df = add_text_features(df, 
+                           text_vars = ['income'], 
+                           tf_idf_feats = 5,
+                           drop = True)
+    assert df.shape[1] == 9
+
+def test_add_text_features_10():
+    data = {'age': [27, np.nan, 30, 25, np.nan], 
+        'height': [170, 168, 173, 177, 165], 
+        'gender': ['female', 'male', np.nan, 'male', 'female'],
+        'income': ['high', 'medium', 'low', 'low', 'no income']}
+    df = pd.DataFrame(data)
+    df = add_text_features(df, 
+                           text_vars = ['income', 'gender'], 
+                           tf_idf_feats = 2,
+                           drop = True)
+    assert df.shape[1] == 10
