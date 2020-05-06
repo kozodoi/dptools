0444af89ca758ba470cc6141e5463df187d464bc
Kozodoi <n.kozodoi@icloud.com>
2020-05-06 18:08:54 +0200

    test: Adding unit tests for encode(factors)
    Adding 3 unit tests

--- dptools/tests/test_feature_engineering.py
+++ dptools/tests/test_feature_engineering.py
@@ -53,3 +53,30 @@ def test_add_text_features_10():
                            tf_idf_feats = 2,
                            drop = True)
     assert df.shape[1] == 10
+
+def test_encode_factors_label():
+    data = {'age': [27, np.nan, 30, 25, np.nan], 
+        'height': [170, 168, 173, 177, 165], 
+        'gender': ['female', 'male', np.nan, 'male', 'female'],
+        'income': ['high', 'medium', 'low', 'low', 'no income']}
+    df = pd.DataFrame(data)
+    df = encode_factors(df, method = 'label')
+    assert df.shape[1] == 4
+
+def test_encode_factors_dummy_8():
+    data = {'age': [27, np.nan, 30, 25, np.nan], 
+        'height': [170, 168, 173, 177, 165], 
+        'gender': ['female', 'male', np.nan, 'male', 'female'],
+        'income': ['high', 'medium', 'low', 'low', 'no income']}
+    df = pd.DataFrame(data)
+    df = encode_factors(df, method = 'dummy')
+    assert df.shape[1] == 8
+
+def test_encode_factors_dummy_7():
+    data = {'age': [27, np.nan, 30, 25, np.nan], 
+        'height': [170, 168, 173, 177, 165], 
+        'gender': ['female', 'male', np.nan, 'male', 'female'],
+        'income': ['high', 'medium', 'low', 'low', 'no income']}
+    df = pd.DataFrame(data)
+    df = encode_factors(df, factors = 'income', method = 'dummy')
+    assert df.shape[1] == 7
