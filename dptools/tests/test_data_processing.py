3b35fb94dea30fedca8be47b8868933e9be6101b
Nikita Kozodoi <n.kozodoi@icloud.com>
2020-07-29 15:27:05 +0200

    test: Remove unit test

--- dptools/tests/test_data_processing.py
+++ dptools/tests/test_data_processing.py
@@ -7,7 +7,6 @@ from dptools import print_missings
 from dptools import fill_missings
 from dptools import print_factor_levels
 from dptools import split_nested_features
-from dptools import correct_colnames
 
 def test_split_nested_features_4():
     data = {'age': [27, np.nan, 30, 25, np.nan], 
@@ -40,13 +39,3 @@ def test_fill_missings_unknown():
     df = pd.DataFrame(data)
     df = fill_missings(df, to_0_cols = 'age', to_unknown_cols = 'income')
     assert df['income'][1] == 'unknown'
-
-def correct_colnames():
-    data = {'age': [27, np.nan, 30, 25, np.nan], 
-        'height': [170, 168, 173, 177, 165], 
-        'height': ['female', 'male', np.nan, 'male', 'female'],
-        'income': ['high', 'medium', 'low', 'low', 'no income']}
-    df = pd.DataFrame(data)
-    df.columns = ['age', 'height', 'height', 'incöme']
-    df = correct_colnames(df)
-    assert all(df.columns == ['age', 'height', 'height_2', 'incme'])
